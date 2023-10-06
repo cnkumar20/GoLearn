a229e654a9b5a6e573c45fe533c426259947d8c9
Chandregowda, Nandiesh Kumar <nchandregowda@conversantmedia.com>
2023-10-06 10:04:26 -0500

    c

---
+++ go.mod
@@ -0,0 +1,4 @@
+module GoLearn
+
+go 1.21
+
---
+++ learn.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+)
+
+func daysOfWork() (x, y string, err error) {
+	x = "Monday"
+	y = "Thursday"
+	err = nil
+	return
+}
+func getlength(name, color string) int {
+	var n = len(name)
+	var c = len(color)
+	return c + n
+}
+
+type student struct {
+	name string
+	age  int
+}
+
+func main() {
+	var num int = 5
+	const name = "kumar"
+	fmt.Println("len:", num, " name:", name)
+	var fmt_name = fmt.Sprintf(name)
+	fmt.Println("name:", fmt_name)
+	msg := 0.55
+	fmt.Printf("%.2f \n", msg)
+	if 3 < 3 {
+		fmt.Print("2 < 3\n")
+	} else if 3 < 4 {
+		fmt.Print("3<4\n")
+	} else {
+		fmt.Printf("None \n")
+	}
+	if length := getlength("Nandiesh", "brown"); length > 1 {
+		fmt.Printf("this is length %d \n", length)
+	}
+	x, y, err := daysOfWork()
+	if err != nil {
+		fmt.Printf("%s , %s \n", x, y)
+	}
+	var s1 student
+	s1.name = "nandiesh"
+	s1.age = 35
+	fmt.Printf("Name : %s \n Age: %d\n", s1.name, s1.age)
+
+	type Rectangle struct {
+		width  int
+		height int
+	}
+	check1 := func(name string) int {
+		i := len(name)
+		return i
+	}
+	fmt.Printf("Length of name :%d \n", check1("Kumar"))
+	slice_ex := make([]int, 6)
+	fmt.Println(slice_ex[:6])
+	slice_ex = append(slice_ex, 6)
+	fmt.Println(len(slice_ex))
+	fmt.Println(slice_ex[2:])
+	n2 := map[string]int{"foo": 1, "bar": 2}
+	n2["kumar"] = 3
+	fmt.Println(n2["kumar"])
+	counts := make(map[string]int)
+	counts["one"] = 1
+	fmt.Printf("One: %d\n", counts["one"])
+	for x := 0; x < 2; x++ {
+		c := ""
+		if x == 0 {
+			c = "one"
+		} else if x == 1 {
+			c = "two"
+		}
+		elem, err := counts[c]
+		if err != true {
+			fmt.Println("Element doesn't exist")
+		}
+		fmt.Printf("Iter elem : %d \n", elem)
+
+	}
+
+}
